7ddd2940efba22d267a0bbc6563f8c6c07ce7caa
unknown <vovanty0504@gmail.comgit config --global user.name vovanty0504>
2022-09-20 15:58:44 +0700

    a

--- 08_validation/execrise/music/src/main/java/com/example/controller/MusicController.java
+++ 08_validation/execrise/music/src/main/java/com/example/controller/MusicController.java
@@ -24,7 +24,7 @@ public class MusicController {
     @GetMapping("/")
     public String showList(Model model) {
         List<Music> musicList = musicService.findAll();
-        model.addAttribute("musicList",musicList);
+        model.addAttribute("musicList", musicList);
         return "list";
     }
 
@@ -35,13 +35,13 @@ public class MusicController {
     }
 
     @PostMapping("/save")
-    public String save(@ModelAttribute @Validated MusicDto musicDto , BindingResult bindingResult,
+    public String save(@ModelAttribute @Validated MusicDto musicDto, BindingResult bindingResult,
                        RedirectAttributes redirectAttributes) {
-        if(bindingResult.hasFieldErrors()){
+        if (bindingResult.hasFieldErrors()) {
             return "create";
-        }else {
+        } else {
             Music music = new Music();
-            BeanUtils.copyProperties(musicDto,music);
+            BeanUtils.copyProperties(musicDto, music);
             musicService.save(music);
             redirectAttributes.addFlashAttribute("mess", "add new successful!");
             return "redirect:/";
@@ -55,34 +55,42 @@ public class MusicController {
     }
 
     @PostMapping("/edit")
-    public String edit(Music music,RedirectAttributes redirectAttributes) {
-        musicService.update( music);
-        redirectAttributes.addFlashAttribute("mess","Update product successful!");
-        return "redirect:/";
+    public String edit(@ModelAttribute("musicList") @Validated MusicDto musicDto, BindingResult bindingResult,
+                       RedirectAttributes redirectAttributes) {
+
+        if (bindingResult.hasFieldErrors()) {
+            return "edit";
+        } else {
+            Music music = new Music();
+            BeanUtils.copyProperties(musicDto, music);
+            musicService.update(music);
+            redirectAttributes.addFlashAttribute("mess", "add new successful!");
+            return "redirect:/";
+        }
     }
 
     @GetMapping("/{id}/delete")
-    public String delete(@PathVariable int id , Model model){
-        model.addAttribute("musicList",musicService.findById(id));
+    public String delete(@PathVariable int id, Model model) {
+        model.addAttribute("musicList", musicService.findById(id));
         return "delete";
     }
 
     @PostMapping("/delete")
-    public String delete(Music music, RedirectAttributes redirectAttributes){
+    public String delete(Music music, RedirectAttributes redirectAttributes) {
         musicService.remove(music);
-        redirectAttributes.addFlashAttribute("mess","Removed customer successfully!");
+        redirectAttributes.addFlashAttribute("mess", "Removed customer successfully!");
         return "redirect:/";
     }
 
     @GetMapping("/{id}/view")
-    public String view(@PathVariable int id, Model model){
-        model.addAttribute("musicList",musicService.findById(id));
+    public String view(@PathVariable int id, Model model) {
+        model.addAttribute("musicList", musicService.findById(id));
         return "view";
     }
 
-    @GetMapping ("/search")
-    public String search(@RequestParam String name , Model model){
-        model.addAttribute("musicList",musicService.findName(name));
+    @GetMapping("/search")
+    public String search(@RequestParam String name, Model model) {
+        model.addAttribute("musicList", musicService.findName(name));
         return "list";
     }
 }
