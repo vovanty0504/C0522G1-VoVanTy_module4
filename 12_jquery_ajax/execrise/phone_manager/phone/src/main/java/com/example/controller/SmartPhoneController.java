1d9bfb6024cf1f627d47fd20545de6cdf0b29d9a
unknown <vovanty0504@gmail.comgit config --global user.name vovanty0504>
2022-09-29 22:23:50 +0700

    a

--- 12_jquery_ajax/execrise/phone_manager/phone/src/main/java/com/example/controller/SmartPhoneController.java
+++ 12_jquery_ajax/execrise/phone_manager/phone/src/main/java/com/example/controller/SmartPhoneController.java
@@ -7,28 +7,40 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.ModelAndView;
 
-import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/smartphone")
+@RequestMapping("/smartphones")
 @CrossOrigin("*")
 public class SmartPhoneController {
     @Autowired
     private IPhoneService phoneService;
+
 
     @PostMapping
     public ResponseEntity<SmartPhone> createSmartphone(@RequestBody SmartPhone smartphone) {
         return new ResponseEntity<>(phoneService.save(smartphone), HttpStatus.CREATED);
     }
 
-    @GetMapping()
-    public ResponseEntity<List<SmartPhone>> allPhones() {
+    @GetMapping("/{id}")
+    public ResponseEntity<SmartPhone> findById(@PathVariable Long id) {
+        if (!phoneService.findById(id).isPresent()){
+            return new ResponseEntity<>(phoneService.findById(id).get(), HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<>(phoneService.findById(id).get(), HttpStatus.OK);
+
+    }
+
+    @GetMapping
+    public ResponseEntity<Iterable<SmartPhone>> allPhones() {
         return new ResponseEntity<>(phoneService.findAll(), HttpStatus.OK);
     }
 
+    @GetMapping("/list")
+    public ResponseEntity<Iterable<SmartPhone>> getAllSmartphonePage() {
+        return new ResponseEntity<>(phoneService.findAll(), HttpStatus.OK);
+    }
 
     @DeleteMapping("/{id}")
     public ResponseEntity<SmartPhone> deleteSmartphone(@PathVariable Long id) {
@@ -40,4 +52,18 @@ public class SmartPhoneController {
         return new ResponseEntity<>(smartphoneOptional.get(), HttpStatus.NO_CONTENT);
     }
 
+    @PutMapping("/{id}")
+    public ResponseEntity<SmartPhone> updateBlog(@PathVariable Long id,
+                                                 @RequestBody SmartPhone smartphone) {
+        SmartPhone currentSmartphone = phoneService.findById(id).get();
+        if (currentSmartphone == null) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        currentSmartphone.setModel(smartphone.getModel());
+        currentSmartphone.setPrice(smartphone.getPrice());
+        currentSmartphone.setProducer(smartphone.getProducer());
+
+        phoneService.save(currentSmartphone);
+        return new ResponseEntity<>(currentSmartphone, HttpStatus.OK);
+    }
 }
